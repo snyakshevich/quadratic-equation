2baf76d19c7bb873bcba8cc0585d81266d7c1b11
snyakshevich <svetlana_2008@mail.ru>
2018-09-30 22:03:41 +0300

    roots

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports = function solveEquation(a,b,c) {
+     var d=b*b-4*a*c; // дискриминант
+
+    if ( d<0 )
+        return []; // нет корней
+
+    if ( d==0 )
+        return [ -b/(2*a) ]; // один корень
+
+    var x1=(-b+Math.sqrt(d))/(2*a);
+    var x2=(-b-Math.sqrt(d))/(2*a);
+    return [ x1, x2 ]; // два корня
 }
+var result=squareRoots(8,9,4);
+console.log(result);
